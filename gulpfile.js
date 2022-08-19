613de489f0659111471a39ccf36afacff221d280
Pavel Yashchenko <yashchenkops@gmail.com>
2022-08-19 21:43:41 +0300

    added dropdown

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,8 @@ gulp.task('scss', function(){
 gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css'
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/selectric/public/selectric.css'
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -45,7 +46,8 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/selectric/public/jquery.selectric.min.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
